95ff15a20ead7970c493bc59953a5b432e91c677
Tomer Dore <tomerdore963@gmail.com>
2021-01-31 00:31:01 +0200

    finish version 1

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import AirbnbLogo from './assets/Airbnb-Logo.svg';
 import LanguageIcon from '@material-ui/icons/Language';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -11,7 +12,9 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-      <img src={AirbnbLogo} alt='Airbnb Logo' className='header__logo' />
+      <Link to='/'>
+        <img src={AirbnbLogo} alt='Airbnb Logo' className='header__logo' />
+      </Link>
 
       <div className='header__center'>
         <input type='text' />
